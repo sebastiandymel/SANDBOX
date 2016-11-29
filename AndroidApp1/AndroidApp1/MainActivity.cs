a2df2a910edf5f7abe19240261ad83a15defe4c1
sebastiandymel <dymel@o2.pl>
2016-11-29 22:42:13 +0100

    android 2

--- AndroidApp1/AndroidApp1/MainActivity.cs
+++ AndroidApp1/AndroidApp1/MainActivity.cs
@@ -1,6 +1,12 @@
-﻿using Android.App;
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Graphics;
 using Android.Widget;
 using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
 
 namespace AndroidApp1
 {
@@ -13,6 +19,55 @@ namespace AndroidApp1
 
             // Set our view from the "main" layout resource
             // SetContentView (Resource.Layout.Main);
+            var button = FindViewById<Button>(Resource.Id.button1);
+            button.Click += OnButtonClick;
+        }
+
+        private void OnButtonClick(object sender, EventArgs e)
+        {
+            // var view = FindViewById<View>(Resource.Id.view1);
+            
+        }
+    }
+
+    public class RenderView : View
+    {
+        private Paint paint;
+
+        public RenderView(IntPtr javaReference, JniHandleOwnership transfer) : base(javaReference, transfer)
+        {
+        }
+
+        public RenderView(Context context) : base(context)
+        {
+            paint = new Paint();
+        }
+
+        public RenderView(Context context, IAttributeSet attrs) : base(context, attrs)
+        {
+        }
+
+        public RenderView(Context context, IAttributeSet attrs, int defStyleAttr) : base(context, attrs, defStyleAttr)
+        {
+        }
+
+        public RenderView(Context context, IAttributeSet attrs, int defStyleAttr, int defStyleRes) : base(context, attrs, defStyleAttr, defStyleRes)
+        {
+        }
+
+        protected void onDraw(Canvas canvas)
+        {
+            base.OnDraw(canvas);
+            int x = Width;
+            int y = Height;
+            int radius;
+            radius = 100;
+            paint.SetStyle(Paint.Style.Fill);
+            paint.Color = Color.Wheat;
+            canvas.DrawPaint(paint);
+            // Use Color.parseColor to define HTML colors
+            paint.Color = Color.ParseColor("#CD5C5C");
+            canvas.DrawCircle(x / 2, y / 2, radius, paint);
         }
     }
 }
--- AndroidApp1/AndroidApp1/Resources/Resource.Designer.cs
+++ AndroidApp1/AndroidApp1/Resources/Resource.Designer.cs
@@ -57,6 +57,25 @@ namespace AndroidApp1
 			}
 		}
 		
+		public partial class Id
+		{
+			
+			// aapt resource value: 0x7f050000
+			public const int button1 = 2131034112;
+			
+			// aapt resource value: 0x7f050001
+			public const int view1 = 2131034113;
+			
+			static Id()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Id()
+			{
+			}
+		}
+		
 		public partial class Layout
 		{
 			
