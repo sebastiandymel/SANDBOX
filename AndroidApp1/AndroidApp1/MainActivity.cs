f438c7de9344cf37414db95f2fcc9e09d2340812
sebastiandymel <dymel@o2.pl>
2016-11-30 06:23:08 +0100

    refactor

--- AndroidApp1/AndroidApp1/MainActivity.cs
+++ AndroidApp1/AndroidApp1/MainActivity.cs
@@ -1,12 +1,7 @@
 ﻿using System;
 using Android.App;
-using Android.Content;
-using Android.Graphics;
 using Android.Widget;
 using Android.OS;
-using Android.Runtime;
-using Android.Util;
-using Android.Views;
 
 namespace AndroidApp1
 {
@@ -27,47 +22,6 @@ namespace AndroidApp1
         {
             // var view = FindViewById<View>(Resource.Id.view1);
             
-        }
-    }
-
-    public class RenderView : View
-    {
-        private Paint paint;
-
-        public RenderView(IntPtr javaReference, JniHandleOwnership transfer) : base(javaReference, transfer)
-        {
-        }
-
-        public RenderView(Context context) : base(context)
-        {
-            paint = new Paint();
-        }
-
-        public RenderView(Context context, IAttributeSet attrs) : base(context, attrs)
-        {
-        }
-
-        public RenderView(Context context, IAttributeSet attrs, int defStyleAttr) : base(context, attrs, defStyleAttr)
-        {
-        }
-
-        public RenderView(Context context, IAttributeSet attrs, int defStyleAttr, int defStyleRes) : base(context, attrs, defStyleAttr, defStyleRes)
-        {
-        }
-
-        protected void onDraw(Canvas canvas)
-        {
-            base.OnDraw(canvas);
-            int x = Width;
-            int y = Height;
-            int radius;
-            radius = 100;
-            paint.SetStyle(Paint.Style.Fill);
-            paint.Color = Color.Wheat;
-            canvas.DrawPaint(paint);
-            // Use Color.parseColor to define HTML colors
-            paint.Color = Color.ParseColor("#CD5C5C");
-            canvas.DrawCircle(x / 2, y / 2, radius, paint);
         }
     }
 }
